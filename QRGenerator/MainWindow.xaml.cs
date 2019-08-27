081972ac925a8c785160d659acc3bb6a22b5cde1
pjamenaja <pjame.fb@gmail.com>
2019-08-28 01:01:13 +0700

    PromptPay QR generator

---
+++ QRGenerator/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace QRGenerator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtPromptPayID.Text = "3100600229665";
+            txtAmount.Text = "1000";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            uctlQRGenerator.GenerateQR();
+        }
+    }
+}
