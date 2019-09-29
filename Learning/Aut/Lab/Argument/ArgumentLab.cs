276f23ec5b985c17aef21cbbfe80aafa6c196a01
Auttz <auttzmargaretz@gmail.com>
2019-09-29 20:08:15 +0700

    Added lab 001

---
+++ Learning/Aut/Lab/Argument/ArgumentLab.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Aut.Lab.Argument
+{
+    public class ArgumentLab
+    {
+        public ArgumentLab(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public ArgumentLab()
+        {
+            Console.WriteLine("In ArgumentLab");
+        }
+    }
+}
---
+++ Learning/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Aut.Lab.Argument;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            if (args.Length <= 1)
+            {
+               Console.WriteLine("Please enter sub-command");
+               return;
+            }
+            
+            string cmd = args[0];
+            string param = args[1];      
+
+            if (cmd.Equals("echo"))
+            {
+                ArgumentLab lab01 = new ArgumentLab(param);
+            }
+        }
+    }
+}
