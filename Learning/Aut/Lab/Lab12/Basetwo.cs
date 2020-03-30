8e7180ceb4788c1f1f9af05f528fe477a8d24363
Auttz <auttzmargaretz@gmail.com>
2020-03-30 20:45:43 +0700

    Added is Base2valid method

--- Learning/Aut/Lab/Lab12/Basetwo.cs
+++ Learning/Aut/Lab/Lab12/Basetwo.cs
@@ -12,10 +12,30 @@ namespace Aut.Lab.Lab12
             inputnum = num;   
         }
         
-        
+        private bool isBase2Valid(string baseTwoStr)
+        {
+            char[] arrch = baseTwoStr.ToCharArray();
+            for(int i = 0;i<arrch.Length;i++)
+            {
+                if(arrch[i] != '0' && arrch[i] != '1')
+                {
+                    return false;
+                }             
+            }
+            return true;
+            
+        }
+
         public void ChangeBaseTwo()
         {            
             char[] arrchar = inputnum.ToCharArray();           
+
+            if (!isBase2Valid(inputnum))
+            {
+                Console.WriteLine("Error !!");
+                return;
+            }
+  
             Int64 sum = 0;
             int len = arrchar.Length;
             for (int i = 1; i <= len; i++)
@@ -23,14 +43,9 @@ namespace Aut.Lab.Lab12
                 char ch = arrchar[i-1];
                 int x = Convert.ToInt32(ch.ToString());
                 sum = sum + (Int64) Math.Pow(2, len-i) * x;
-                if(arrchar[i-1] != '0' && arrchar[i-1] !='1')
-                {
-                    Console.WriteLine("Error !!");
-                }           
             }
 
-            Console.WriteLine("Output = {0}", sum);
-            
+            Console.WriteLine("Output = {0}", sum);                    
         }
         
     }
